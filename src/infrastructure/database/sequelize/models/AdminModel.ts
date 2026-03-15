65930ce89a2aa6dd1a8f5641aeb563c05d22ee8e
Gustavo <gustavocesargevigi@gmail.com>
2026-03-15 12:01:37 -0300

    feat Administrador(Admin): Implementação da entidade, repositórios, controller, e CreateAdminService.ts. Necessário desenvolver restante das funcionalidades

--- src/infrastructure/database/sequelize/models/AdminModel.ts
+++ src/infrastructure/database/sequelize/models/AdminModel.ts
@@ -14,3 +14,16 @@ class AdminModel
   public email!: string;
   public password!: string;
 }
+
+AdminModel.init(
+  {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    cpf: { type: DataTypes.STRING(14), allowNull: false, unique: true },
+    name: { type: DataTypes.STRING(100), allowNull: false },
+    email: { type: DataTypes.STRING(100), allowNull: false, unique: true },
+    password: { type: DataTypes.STRING(100), allowNull: false },
+  },
+  { sequelize, tableName: 'Advisors', timestamps: true },
+);
+
+export default AdminModel;
