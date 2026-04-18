09fe58c733d6ac8899b45c88bec75e44b8d79ce9
LeoCPassos <leo-cpassos@hotmail.com>
2026-04-18 17:48:10 -0300

    Rota get student by id do endpoint Student documentada no swagger

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -3,6 +3,36 @@ import { makeStudentController } from '../main/factories/makeStudentController';
 
 const studentRouter = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Students
+ *     description: Students API endpoint
+ */
+
+/**
+ * @swagger
+ * /students/{id}:
+ *   get:
+ *     tags:
+ *       - Students
+ *     summary: Get student by ID.
+ *     description: Get student by ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: Student ID
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '404':
+ *         description: Student not found
+ *       '500':
+ *         description: Internal server error
+ */
 studentRouter.get('/:id', (req, res) => {
   return makeStudentController().getById(req, res);
 });
