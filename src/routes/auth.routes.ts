d8f16e1905d3a1c7d40d0cbd2e0bffca887a4ef7
LeoCPassos <leo-cpassos@hotmail.com>
2026-04-19 15:25:58 -0300

    Documentado endpoints Auth

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -5,10 +5,103 @@ const authRouter = Router();
 
 const authController = AuthFactory.create();
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Auth
+ *     description: Autentificação e Recuperação de Senha
+ */
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     tags:
+ *       - Auth
+ *     summary: Login with E-mail and password.
+ *     description: Login with E-mail and password.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           description: User login credentials
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '401':
+ *         description: Invalid email or password
+ *       '500':
+ *         description: Internal server error
+ */
 authRouter.post('/login', (req, res) => authController.login(req, res));
+
+/**
+ * @swagger
+ * /auth/forgot-password:
+ *   post:
+ *     tags:
+ *       - Auth
+ *     summary: Request password reset.
+ *     description: Request password reset.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           description: User login email
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '401':
+ *         description: Invalid email
+ *       '500':
+ *         description: Internal server error
+ */
 authRouter.post('/forgot-password', (req, res) =>
   authController.forgotPassword(req, res),
 );
+
+/* ======= TODO: implementar o reset de senha ======= */
+
+/**
+ * @swagger
+ * /auth/reset-password/?token={token}:
+ *   get:
+ *     tags:
+ *       - Auth
+ *     summary: Password reset.
+ *     description: Password reset.
+ *     parameters:
+ *       - in: query
+ *         name: token
+ *         schema:
+ *           type: string
+ *         description: Reset password token
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '500':
+ *         description: Internal server error
+ */
 authRouter.get('/reset-password', (req, res) => {
   const { token } = req.query;
   return res.json({
