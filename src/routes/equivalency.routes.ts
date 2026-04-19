5262d9280087c461e24eca88aeeff5bbb0c91266
LeoCPassos <leo-cpassos@hotmail.com>
2026-04-19 15:47:46 +0000

    Documentado endpoints de equivalencies

--- src/routes/equivalency.routes.ts
+++ src/routes/equivalency.routes.ts
@@ -4,22 +4,161 @@ import { authMiddleware } from '../infrastructure/http/middlewares/AuthMiddlewar
 
 const equivalencyRouter = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Equivalency
+ *     description: Equivalency API endpoint
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Equivalency:
+ *       type: object
+ *       required:
+ *          - name
+ *       properties:
+ *         name:
+ *           type: string
+ *         description:
+ *           type: string
+ *         courseId:
+ *           type: integer
+ */
+
+/**
+ * @swagger
+ * /equivalencies/:
+ *   get:
+ *     tags:
+ *       - Equivalency
+ *     summary: List all equivalencies.
+ *     description: List all equivalencies.
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '404':
+ *         description: Equivalency not found
+ *       '500':
+ *         description: Internal server error
+ */
 equivalencyRouter.get('/', (req, res) => {
   return makeEquivalencyController().findAll(req, res);
 });
 
+/**
+ * @swagger
+ * /equivalencies/search?name={name}:
+ *   get:
+ *     tags:
+ *       - Equivalency
+ *     summary: Get equivalency by name.
+ *     description: Get equivalency by name.
+ *     parameters:
+ *       - in: query
+ *         name: name
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Equivalency name
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '404':
+ *         description: Equivalency not found
+ *       '500':
+ *         description: Internal server error
+ */
 equivalencyRouter.get('/search', (req, res) => {
   return makeEquivalencyController().findByName(req, res);
 });
 
+/* ======= TODO: colocar token no /eqyuvalencies/{id} ======= */
+
+/**
+ * @swagger
+ * /equivalencies/{id}:
+ *   get:
+ *     tags:
+ *       - Equivalency
+ *     summary: Get equivalency by ID.
+ *     description: Get equivalency by ID. (Precisa adicionar token)
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: Equivalency ID
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '404':
+ *         description: Equivalency not found
+ *       '500':
+ *         description: Internal server error
+ */
 equivalencyRouter.get('/:id', authMiddleware, (req, res) => {
   return makeEquivalencyController().findById(req, res);
 });
 
+/**
+ * @swagger
+ * /equivalencies/create:
+ *   post:
+ *     tags:
+ *       - Equivalency
+ *     summary: Create equivalency.
+ *     description: Create equivalency.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           description: Create equivalency
+ *           schema:
+ *             $ref: '#/components/schemas/Equivalency'
+ *     responses:
+ *       '201':
+ *         description: Created equivalency successfully
+ *       '400':
+ *         description: Bad request
+ *       '500':
+ *         description: Internal server error
+ */
 equivalencyRouter.post('/create', (req, res) => {
   return makeEquivalencyController().create(req, res);
 });
 
+/**
+ * @swagger
+ * /equivalencies/updated/{id}:
+ *   patch:
+ *     tags:
+ *       - Equivalency
+ *     summary: Update equivalency.
+ *     description: Update equivalency.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           description: Update equivalency
+ *           schema:
+ *             $ref: '#/components/schemas/Equivalency'
+ *     responses:
+ *       '201':
+ *         description: update equivalency successfully
+ *       '400':
+ *         description: Bad request
+ *       '500':
+ *         description: Internal server error
+ */
 equivalencyRouter.patch('/updated/:id', (req, res) => {
   return makeEquivalencyController().update(req, res);
 });
