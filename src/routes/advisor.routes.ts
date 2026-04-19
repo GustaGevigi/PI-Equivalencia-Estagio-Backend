d8a389bd8e4fb27b39cd8362317bda581df08227
LeoCPassos <leo-cpassos@hotmail.com>
2026-04-18 22:23:13 -0300

    Documentado endpoints de Advisors

--- src/routes/advisor.routes.ts
+++ src/routes/advisor.routes.ts
@@ -3,18 +3,140 @@ import { MakeAdvisorController } from '../main/factories/makeAdvisorController';
 
 const advisorRouter = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Advisors
+ *     description: Advisor API endpoint
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Advisor:
+ *       type: object
+ *       required:
+ *          - name
+ *          - email
+ *          - password
+ *          - cpf
+ *       properties:
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *           format: email
+ *         password:
+ *           type: string
+ *         cpf:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * /advisors/{id}:
+ *   get:
+ *     tags:
+ *       - Advisors
+ *     summary: Get advisor by ID.
+ *     description: Get advisor by ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: Advisor ID
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '404':
+ *         description: Advisor not found
+ *       '500':
+ *         description: Internal server error
+ */
 advisorRouter.get('/:id', (req, res) => {
   return MakeAdvisorController().getById(req, res);
 });
 
+/**
+ * @swagger
+ * /advisors/search/cpf?cpf={cpf}:
+ *   get:
+ *     tags:
+ *       - Advisors
+ *     summary: Get advisor by CPF.
+ *     description: Get advisor by CPF.
+ *     parameters:
+ *       - in: query
+ *         name: cpf
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Advisor CPF
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '404':
+ *         description: Advisor not found
+ *       '500':
+ *         description: Internal server error
+ */
 advisorRouter.get('/search/cpf', (req, res) => {
   return MakeAdvisorController().getByCpf(req, res);
 });
 
+/**
+ * @swagger
+ * /advisors/search/email?email={email}:
+ *   get:
+ *     tags:
+ *       - Advisors
+ *     summary: Get advisor by E-mail.
+ *     description: Get advisor by E-mail.
+ *     parameters:
+ *       - in: query
+ *         name: email
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Advisor E-mail
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '404':
+ *         description: Advisor not found
+ *       '500':
+ *         description: Internal server error
+ */
 advisorRouter.get('/search/email', (req, res) => {
   return MakeAdvisorController().getByEmail(req, res);
 });
 
+/**
+ * @swagger
+ * /advisors/:
+ *   post:
+ *     tags:
+ *       - Advisors
+ *     summary: Create advisor.
+ *     description: Create Advisor.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           description: Create advisor
+ *           schema:
+ *             $ref: '#/components/schemas/Advisor'
+ *     responses:
+ *       '201':
+ *         description: Created advisor successfully
+ *       '400':
+ *         description: Bad request
+ *       '500':
+ *         description: Internal server error
+ */
 advisorRouter.post('/', (req, res) => {
   return MakeAdvisorController().create(req, res);
 });
